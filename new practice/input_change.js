837926568cab1a33f95bcf706612fa0d0780a897
UTKARSH232003 <92358948+UTKARSH232003@users.noreply.github.com>
2023-03-21 15:44:34 +0530

    Delete new practice directory

--- new practice/input_change.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('input');
-const h1 = document.querySelectorAll('h1');
-
-input.addEventListener('change', function(e){
-    console.log("INPUT EVENT!");
-    // console.log(e);
-})
-
